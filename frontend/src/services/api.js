ef8bd7f8e61532fa4f28d158cccb05cd86f88083
nikunjjadav123 <dirensj@gmail.com>
2025-11-23 21:47:28 +0530

    Prepare for Railway Plateform  deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "../store/auth";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api",
 });
 
 api.interceptors.request.use((config) => {
