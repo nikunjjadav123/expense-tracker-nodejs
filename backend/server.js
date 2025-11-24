234fd5696241f0cda257349f417a3de7bd932e26
nikunjjadav123 <dirensj@gmail.com>
2025-11-24 18:47:39 +0530

    Added cors script in server.js file

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://expense-tracker-production-9079.up.railway.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", require("./src/routes/authRoutes"));
